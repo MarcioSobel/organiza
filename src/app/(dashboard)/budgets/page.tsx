d4ad75421f2a1b9d7faf1555f585f304b4cae288
marciosobel <marcinhosobel@hotmail.com>
2024-08-26 05:07:19 -0300

    :wrench: chore: muda o nome dos componentes do dashboard

--- src/app/(dashboard)/budgets/page.tsx
+++ src/app/(dashboard)/budgets/page.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
+export default function Budgets() {
   return <p>Budgets</p>;
 }
--- src/app/(dashboard)/investments/page.tsx
+++ src/app/(dashboard)/investments/page.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
+export default function Investments() {
   return <p>Investments</p>;
 }
