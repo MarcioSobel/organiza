4ff1f50c22493636b36409a5723c694ef10bd104
carlinaceo28 <carlosalbertoneto28@gmail.com>
2024-09-12 15:08:22 -0300

    :sparkles:feat:Gráfico Orçamento

--- src/app/(dashboard)/budgets/page.tsx
+++ src/app/(dashboard)/budgets/page.tsx
@@ -1,6 +1,20 @@
-export default function Budgets() {
-  return <p>Budgets
+import { StandardDefinition } from '@phosphor-icons/react'
+import styles from './Budgets.module.css'
+import { TesteGraf } from '@/app/(dashboard)/budgets/viewsTest'
 
-    Carlinhos e Biel começaram os processos por aqui.
-  </p>;
+
+
+export default function Budgets() {
+  return (
+    <section className={styles.budgets_container}>
+      <h1>
+        <span>Orçamento</span>
+        </h1>
+      <p>Comece a gerenciar os seus Orçamento e planejamentos financeiros!</p>
+        <TesteGraf/>
+    </section>
+  
+
+  
+  )
 }
