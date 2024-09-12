b7772346a2319a72ce672372fd359dfd4c1d6cde
carlinaceo28 <carlosalbertoneto28@gmail.com>
2024-09-12 17:37:26 -0300

    :wrench:Fix:Graficos

--- src/app/(dashboard)/budgets/viewsTest.tsx
+++ src/app/(dashboard)/budgets/viewsTest.tsx
@@ -1,36 +1,50 @@
 "use client"
 
-import { Bar, BarChart } from "recharts"
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
 
-import { ChartConfig, ChartContainer } from "@/components/ui/chart"
+import { ChartConfig, ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
 
 const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
+  { month: "January", Orçamento: 186, Gasto: 80 },
+  { month: "February", Orçamento: 305, Gasto: 200 },
+  { month: "March", Orçamento: 237, Gasto: 120 },
+  { month: "April", Orçamento: 73, Gasto: 190 },
+  { month: "May", Orçamento: 209, Gasto: 130 },
+  { month: "June", Orçamento: 214, Gasto: 140 },
 ]
 
 const chartConfig = {
-  desktop: {
-    label: "Desktop",
+    Orçamento: {
+    label: "Orçamento(R$)",
     color: "#2563eb",
   },
-  mobile: {
-    label: "Mobile",
+  Gasto: {
+    label: "Gasto(R$)",
     color: "#60a5fa",
   },
 } satisfies ChartConfig
 
 export function TesteGraf() {
   return (
-    <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-      <BarChart accessibilityLayer data={chartData}>
-        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-        <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
-      </BarChart>
-    </ChartContainer>
+    <ChartContainer config={chartConfig} className="h-[200px] w-full">
+  <BarChart accessibilityLayer data={chartData}>
+    <CartesianGrid vertical={false} />
+    <XAxis
+      dataKey="month"
+      tickLine={false}
+      tickMargin={10}
+      axisLine={false}
+      tickFormatter={(value) => value.slice(0, 3)}
+    />
+    <ChartTooltip content={<ChartTooltipContent />} />
+    <ChartLegend content={<ChartLegendContent />} />
+    <Bar dataKey="Orçamento" fill="var(--color-Orçamento)" radius={4} />
+    <Bar dataKey="Gasto" fill="var(--color-Gasto)" radius={4} />
+  </BarChart>
+</ChartContainer>
   )
 }
+
+
+
+
