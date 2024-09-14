ec809ff1c8a5e2cd5b84a6e8ce1c6def927d86b2
marciosobel <marcinhosobel@hotmail.com>
2024-09-14 10:10:27 -0300

    :sparkles: feat: adiciona validação de entrada na tela de registro

--- src/app/(auth)/register/_components/card-register.tsx
+++ src/app/(auth)/register/_components/card-register.tsx
@@ -10,75 +10,133 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import { useToast } from "@/hooks/use-toast";
+import api from "@/utils/api/api";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const registerUserSchema = z
+  .object({
+    name: z.string().min(1, "Obrigatório."),
+    surname: z.string().nullable().default(null),
+    email: z.string().email("E-mail inválido."),
+    password: z.string().min(3, "Insira uma senha maior."),
+    confirmPassword: z.string(),
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: "As senhas não coincidem.",
+    path: ["confirmPassword"],
+  });
+
+type RegisterUserSchema = z.infer<typeof registerUserSchema>;
 
 export function CardRegister() {
   const [isHovered, setIsHovered] = useState(false);
-  const updateIsHovered = () => setIsHovered((prev) => !prev);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterUserSchema>({
+    resolver: zodResolver(registerUserSchema),
+  });
 
   const router = useRouter();
-
-  const createAccount = () => {
-    router.push("/home");
-  };
+  const { toast } = useToast();
 
   const goToLoginPage = () => {
     router.push("/login");
   };
 
+  function handleCreateAccount(data: RegisterUserSchema) {
+    api.createUser(data).catch(() => {
+      toast({
+        title: "Usuário já cadastrado.",
+        description: "Este e-mail já está sendo utilizado.",
+        variant: "destructive",
+      });
+    });
+
+    goToLoginPage();
+  }
+
+  const updateIsHovered = () => setIsHovered((prev) => !prev);
+
   return (
-    <Card
-      className="bg-off-white border-off-black border-2 min-w-fit"
-      style={{ boxShadow: "2px 2px #2C2C2C" }}
-    >
-      <CardHeader className="pb-0 pt-6 px-3 items-center font-roboto-slab">
-        <CardTitle className="text-off-black">Crie sua conta</CardTitle>
-      </CardHeader>
+    <form onSubmit={handleSubmit(handleCreateAccount)}>
+      <Card
+        className="bg-off-white border-off-black border-2 min-w-fit"
+        style={{ boxShadow: "2px 2px #2C2C2C" }}
+      >
+        <CardHeader className="pb-0 pt-6 px-3 items-center font-roboto-slab">
+          <CardTitle className="text-off-black">Crie sua conta</CardTitle>
+        </CardHeader>
 
-      <div className="flex justify-center my-6">
-        <Separator
-          orientation="horizontal"
-          className="bg-off-grey/50 w-[90%]"
-        />
-      </div>
-
-      <CardContent className="pb-4">
-        <div className="flex gap-4">
-          <InputBar label="Nome" />
-          <InputBar label="Sobrenome" />
+        <div className="flex justify-center my-6">
+          <Separator
+            orientation="horizontal"
+            className="bg-off-grey/50 w-[90%]"
+          />
         </div>
-        <InputBar label="E-mail" type="email" />
-        <InputBar label="Senha" type="password" />
-        <InputBar label="Confirmar senha" type="password" />
-      </CardContent>
 
-      <CardFooter className="flex flex-col justify-center gap-2">
-        <Button className="w-full active:bg-black" onClick={createAccount}>
-          Criar conta
-        </Button>
-
-        <div className="flex items-baseline">
-          <p className="whitespace-nowrap">Já possui uma conta?&nbsp;</p>
-
-          <div
-            className="flex flex-col group cursor-pointer"
-            onMouseEnter={updateIsHovered}
-            onMouseLeave={updateIsHovered}
-            onClick={goToLoginPage}
-          >
-            <Button
-              variant="link"
-              className="p-0 h-fit text-base font-bold hover:no-underline"
-            >
-              Faça seu login aqui.
-            </Button>
-            <div
-              className={`w-0 h-px bg-off-black ${isHovered ? "self-start" : "self-end"} transition-[width] group-hover:w-full`}
+        <CardContent className="pb-4">
+          <div className="flex gap-4">
+            <InputBar
+              label="Nome"
+              register={register("name")}
+              errors={errors}
             />
+            <InputBar label="Sobrenome" register={register("surname")} />
           </div>
-        </div>
-      </CardFooter>
-    </Card>
+          <InputBar
+            label="E-mail"
+            type="email"
+            register={register("email")}
+            errors={errors}
+          />
+          <InputBar
+            label="Senha"
+            type="password"
+            register={register("password")}
+            errors={errors}
+          />
+          <InputBar
+            label="Confirmar senha"
+            type="password"
+            register={register("confirmPassword")}
+            errors={errors}
+          />
+        </CardContent>
+
+        <CardFooter className="flex flex-col justify-center gap-2">
+          <Button className="w-full active:bg-black" type="submit">
+            Criar conta
+          </Button>
+
+          <div className="flex items-baseline">
+            <p className="whitespace-nowrap">Já possui uma conta?&nbsp;</p>
+
+            <div
+              className="flex flex-col group cursor-pointer"
+              onMouseEnter={updateIsHovered}
+              onMouseLeave={updateIsHovered}
+              onClick={goToLoginPage}
+            >
+              <Button
+                variant="link"
+                className="p-0 h-fit text-base font-bold hover:no-underline"
+              >
+                Faça seu login aqui.
+              </Button>
+              <div
+                className={`w-0 h-px bg-off-black ${isHovered ? "self-start" : "self-end"} transition-[width] group-hover:w-full`}
+              />
+            </div>
+          </div>
+        </CardFooter>
+      </Card>
+    </form>
   );
 }
--- src/components/input-bar.tsx
+++ src/components/input-bar.tsx
@@ -1,18 +1,44 @@
 import { HTMLInputTypeAttribute, InputHTMLAttributes } from "react";
 import { Input } from "./ui/input";
 import { cn } from "@/lib/utils";
+import { FieldErrors, UseFormRegister } from "react-hook-form";
+import { TriangleAlert } from "lucide-react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   type?: HTMLInputTypeAttribute;
   label?: string;
+  register?: ReturnType<UseFormRegister<object>>;
+  errors?: FieldErrors<object>;
 }
-export function InputBar({ type, label, ...props }: InputProps) {
+export function InputBar({
+  type,
+  label,
+  register,
+  errors,
+  ...props
+}: InputProps) {
+  let error: string | undefined;
+  if (register && errors) {
+    const { name } = register as { name: string };
+    const errorObj = errors as Record<string, { message: string }>;
+    error = errorObj[name]?.message;
+  }
+
   return (
     <div className="py-2">
-      {label && <p className="font-medium">{label}</p>}
+      <div className="flex gap-3 items-center justify-between">
+        {label && <p className="font-medium">{label}</p>}
+        {error && (
+          <div className="flex items-center justify-start gap-1">
+            <TriangleAlert size={20} color="#f3201d" />
+            <p className="text-semantic-no">{error}</p>
+          </div>
+        )}
+      </div>
       <Input
         {...props}
         type={type}
+        {...(register || {})}
         className={cn(
           // disable default ring highlight
           "focus-visible:ring-transparent focus-visible:ring-offset-0",
