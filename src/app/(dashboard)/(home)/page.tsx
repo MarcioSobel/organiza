54fc49f7df04fefdaa16e12c7d86f9132b1ab064
marciosobel <marcinhosobel@hotmail.com>
2024-09-14 10:41:23 -0300

    :sparkles: feat: redireciona usuário caso o mesmo não esteja logado

--- src/app/(dashboard)/(home)/page.tsx
+++ src/app/(dashboard)/(home)/page.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import ReceiptsHistory from "./_components/receipts-history";
+import { useRouter } from "next/navigation";
+import { hasCookie } from "cookies-next";
 
 export default function Home() {
+  const router = useRouter();
+  if (!hasCookie("token")) {
+    // ...
+    setTimeout(() => router.push("/login"), 10);
+  }
+
   return (
     <div>
       <ReceiptsHistory />
