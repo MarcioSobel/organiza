ae03a8b2eb1304457f14eec1dadb3b3552b465c1
MarcioSobel <marcinhosobel@hotmail.com>
2024-11-09 21:59:51 -0300

    :recycle: refactor: make menu open on hover

--- src/app/(dashboard)/_components/sidebar.tsx
+++ src/app/(dashboard)/_components/sidebar.tsx
@@ -7,18 +7,15 @@ import { List as MenuIcon } from "@phosphor-icons/react";
 export function Sidebar() {
   const [isOpened, setIsOpened] = useState(false);
 
-  const handleExpand = () => {
-    setIsOpened((prev) => !prev);
-  };
-
   return (
     <aside
       className={`flex flex-col bg-off-black p-3 gap-8 h-fit sm:h-[100dvh] w-screen ${isOpened ? "sm:w-[236px]" : "sm:w-[74px]"} transition-[width] overflow-hidden text-white`}
+      onMouseEnter={() => setIsOpened(true)}
+      onMouseLeave={() => setIsOpened(false)}
     >
       <div className="sm:flex flex-col justify-center items-center gap-2 hidden">
         <MenuIcon
           className={`cursor-pointer ${isOpened ? "self-end" : ""}`}
-          onClick={handleExpand}
           size={32}
         />
         <Logo showText={isOpened} />
